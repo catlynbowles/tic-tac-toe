abf0ba5ce990a37596a9df681e106e2713abedaa
Catlyn Bowles <bowlescatlyn@gmail.com>
2022-04-20 20:10:12 -0600

    Add class properties

--- game.js
+++ game.js
@@ -1,9 +1,15 @@
 class Game {
-  constructor(players, turn) {
-    this.players = players;
-    this.turn = turn;
+  constructor() {
+    this.playerOne = new Player(1, 'sponge', 0);
+    this.playerTwo = new Player(2, 'star', 0);
+    this.turn = this.playerOne;
     this.win = false;
     this.draw = false;
     this.reset = false;
+  }
+  setGameBoard() {
+    var playerOne = this.playerOne;
+    var playerTwo = this.playerTwo;
+    // will this be more convenient on page load?
   }
 }
---
+++ main.js
@@ -0,0 +1,1 @@
+// does the game board need to start at all empty strings for the boxes?
